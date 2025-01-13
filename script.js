dbb4d72781a070c725495415d2377f2da4aff1fd
Ritch <ritchtopia@gmail.com>
2025-01-13 18:57:55 +0800

    added more functions for checklist

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ class todoItem {
     }
     //UPDATE FUNCTIONS ABOVE CAN BE REFACTORED INTO ONE (keeping the functions above so you can see the improvement)
     updateField(fieldName, newValue){
-        if (this.hasOwnProperty[fieldName]){ //if this object has title,notes,dueDate,etc.
+        if (this.hasOwnProperty(fieldName)){ //if this object has title,notes,dueDate,etc.
             this[fieldName] = newValue; //give this object's property a new value             NOTE: dont use this.fieldName because fieldName is not a property of an object. fieldName is a variable.
         }
         else{
@@ -64,20 +64,28 @@ class todoItem {
     }
 
 
-
     addChecklistItem(checklistItem){
         this.checklist.push(checklistItem); //push checklistItem to checklist array
     }
 
 
-    updateChecklistItem(checklist){
+    updateChecklistItem(checklistItem, newChecklistItem){
         const index = checklist.indexOf(checklistItem);
         if (index !== -1){
-
+            this.checklist[index] = newChecklistItem;
         } 
     }
 
+    displayChecklist(){
+        console.log(this.checklist); //this.checklist is already in the class so no parameters for this function
+    }
 
+    removeChecklistItem(checklistItem){
+        const index = this.checklist.indexOf(checklistItem); //find the index of the item
+        if (index !== -1){ //-1 means if element is not found
+            this.checklist.splice(index , 1); //start at index and remove 1 items.       NOTE: (index,2) means start at index and remove 2 items
+        }
+    }
 
 }
 
@@ -91,26 +99,27 @@ NOTE: CLASSES GOTTA HAVE THEIR SPECIFIC METHODS
 CLI FIRST BEFORE DOM
 
     I should be able to add a todo (CREATE) 
-        A function to add a new to do item DONE
-        A function to add a new project with specific to do items DONE
+        A function to add a new to do item :))))))))))
+        A function to add a new project with specific to do items :))))))))))
 
     I should be able to manipulate the DOM and see these items  (READ) A function to display
-        Function to display one to do in detail DONE
-        Function to display all items DONE
+        Function to display one to do in detail :))))))))))
+        Function to display all items :))))))))))
 
     I should be able to update these (UPDATE)
-        update title
-        update notes
-        update dueDate
-        update priority
+        update title :))))))))))
+        update notes :))))))))))
+        update dueDate :))))))))))
+        update priority :))))))))))
         update checklist
-            A function to add, done
-            mark as done, 
-            remove a checklist item
+            A function to add, :))))))))))
+            a function to read :))))
+            mark as done
+            remove a checklist item :))))))))
 
     I should be able to delete these (DELETE)
-        delete todo item DONE
-        delete checklist item
+        delete todo item :))))))))))
+        delete checklist item 
         delete project
         
     I should be able to see them be sorted by due date //maybe an array of items that are sorted by due date
